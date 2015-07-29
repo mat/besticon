035e118674c61904fa672b201e9e55b8bbe84c69
Matthias Luedtke <github@matthias-luedtke.de>
2015-07-29 08:56:02 +0200

    Move sort helpers into own file.

--- besticon/besticon.go
+++ besticon/besticon.go
@@ -81,11 +81,7 @@ func FetchIcons(siteURL string) ([]Icon, error) {
 
 	icons := fetchAllIcons(links)
 	icons = rejectBrokenIcons(icons)
-
-	// Order when finished: (width/height, bytes, url)
-	sort.Stable(byURL(icons))
-	sort.Stable(byBytes(icons))
-	sort.Stable(sort.Reverse(byWidthHeight(icons)))
+	sortIcons(icons)
 
 	return icons, nil
 }
@@ -353,26 +349,6 @@ func sha1Sum(b []byte) string {
 	bs := hash.Sum(nil)
 	return fmt.Sprintf("%x", bs)
 }
-
-type byWidthHeight []Icon
-
-func (a byWidthHeight) Len() int      { return len(a) }
-func (a byWidthHeight) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a byWidthHeight) Less(i, j int) bool {
-	return (a[i].Width < a[j].Width) || (a[i].Height < a[j].Height)
-}
-
-type byBytes []Icon
-
-func (a byBytes) Len() int           { return len(a) }
-func (a byBytes) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a byBytes) Less(i, j int) bool { return (a[i].Bytes < a[j].Bytes) }
-
-type byURL []Icon
-
-func (a byURL) Len() int           { return len(a) }
-func (a byURL) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a byURL) Less(i, j int) bool { return (a[i].URL < a[j].URL) }
 
 var client *http.Client
 
