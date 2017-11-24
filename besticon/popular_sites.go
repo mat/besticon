cb9d4b321583f6f38f734abbc6b21b220c5e38e0
Matthias Luedtke <matthias.luedtke@gmail.com>
2017-11-24 21:14:19 +0100

    Read /popular sites from POPULAR_SITES

--- besticon/popular_sites.go
+++ besticon/popular_sites.go
@@ -1,37 +1,13 @@
 package besticon
 
+import (
+	"os"
+	"strings"
+)
+
 // PopularSites we might use for examples and testing.
 var PopularSites []string
 
 func init() {
-	PopularSites = []string{
-		"apple.com",
-		"bbc.co.uk",
-		"bing.com",
-		"booking.com",
-		"dropbox.com",
-		"espn.go.com",
-		"etsy.com",
-		"facebook.com",
-		"flickr.com",
-		"github.com",
-		"imgur.com",
-		"instagram.com",
-		"live.com",
-		"mail.ru",
-		"msn.com",
-		"nytimes.com",
-		"outbrain.com",
-		"pinterest.com",
-		"reddit.com",
-		"stackoverflow.com",
-		"t.co",
-		"tumblr.com",
-		"vimeo.com",
-		"walmart.com",
-		"wikipedia.org",
-		"wordpress.com",
-		"yahoo.com",
-		"yelp.com",
-	}
+	PopularSites = strings.Split(os.Getenv("POPULAR_SITES"), ",")
 }
